623d24ef110245e4c3d0b295bab7526e978a2915
Evan Boissonnot <evan@boissonnot.fr>
2020-04-20 14:59:41 +0200

    refacto: preparation de l'architecture du projet

---
+++ Jeu.Administration/Jeu.Administration.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jeu.Administration.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Jeu.Administration/Jeu.Core.DTOs/Droide.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jeu.Core.DTOs
+{
+    class Droide
+    {
+    }
+}
