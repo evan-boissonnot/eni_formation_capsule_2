613ae60b22e53c0e843a44c0a0030c8be016baa1
Evan Boissonnot <evan@boissonnot.fr>
2020-04-21 15:26:15 +0200

    test: validation premier test unitaire

--- Jeu.Administration/Jeu.Tests.Businesses/DroideBusinessUnitTest.cs
+++ Jeu.Administration/Jeu.Tests.Businesses/DroideBusinessUnitTest.cs
@@ -1,14 +1,23 @@
+using Jeu.Core.Businesses;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace Jeu.Tests.Businesses
 {
     public class DroideBusinessUnitTest
     {
-        [Fact]
-        public void Test01DroideBusiness()
+        [Fact(DisplayName = "Test du get list sans paramètre")]
+        public void Test_GetList_Run_Without_Parameters()
         {
+            IDroideBusiness business = new DroideBusiness();
 
+            var list = business.GetList();
+
+            Assert.NotNull(list);
+            Assert.True(list.Count() > 0);
+
+            Assert.False(list.Any(item => item.Id <= 0), "Aucun id ne doit être à 0");
         }
     }
 }
